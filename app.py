0e26f1ef116bc6387bb4bcafa4eb20d837701905
Amaialopez1 <101877668+Amaialopez1@users.noreply.github.com>
2022-09-28 18:33:02 +0200

    merge with Tetiana and refactor

--- app.py
+++ app.py
@@ -1,34 +1,37 @@
-from flask import Flask,redirect, render_template
+from flask import Flask, redirect, render_template
 from Forms.LoginForm import LoginForm
 from Forms.RegisterForm import RegisterForm
 
+app = Flask(__name__, template_folder='templates')
 
-app = Flask(__name__)
 
+@app.get('/')
+def main_template():
+    return render_template('home/pantallaInicial.html')
 
 @app.get('/login')
 def login_template():
     form = LoginForm()
-    return render_template('login.html', form=form)
+    return render_template('auth/login.html', form=form)
 
 
 @app.post('/login')
 def login():
-    form = LoginForm();
-    return render_template('login.html', form = form)      
+    form = LoginForm()
+    return render_template('auth/login.html', form=form)
+
 
 @app.get('/registro')
 def register_template():
     form = RegisterForm()
-    return render_template('register.html', form=form)
+    return render_template('auth/register.html', form=form)
 
 
 @app.post('/registro')
 def register():
     form = RegisterForm()
-    return render_template('register.html', form = form)          
+    return render_template('auth/register.html', form=form)
+
 
 if __name__ == '__main__':
     app.run()
-
- 
