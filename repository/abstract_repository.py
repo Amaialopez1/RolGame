882feb60f8f55ec5024cd240ff95714f0769b373
Amaialopez1 <101877668+Amaialopez1@users.noreply.github.com>
2022-10-10 12:41:39 +0200

    arreglados fallos

---
+++ repository/abstract_repository.py
@@ -0,0 +1,20 @@
+import abc
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models.base import Base
+
+
+class AbstractRepository(abc.ABC):
+    engine = create_engine(os.environ.get('DATABASE_URL'))
+    Base.metadata.create_all(engine, chekfirst=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    def get(self, entity_id):
+        raise NotImplementedError
+
+    def list(self):
+        raise NotImplementedError
---
+++ repository/attack_repository.py
@@ -0,0 +1,10 @@
+from models.base import Attack
+from repository.abstract_repository import AbstractRepository
+
+
+class AttackRepository(AbstrackRepository):
+    def get(self, attack_id):
+        return self.session.query(Attack).filter_by(id=attack_id).one_or_none()
+
+
+
