b8b8211c6c927c17fac28588bc976c3f03c16f3b
Amaialopez1 <101877668+Amaialopez1@users.noreply.github.com>
2022-10-11 22:35:20 +0200

    cambiar el tipo de letra y tambien modificar el tamaño

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from re import L
-from flask import Flask, redirect, render_template,request, url_for, session
+from flask import Flask, redirect, render_template, request, url_for, session
 from itsdangerous import NoneAlgorithm
 from numpy import void
 from Forms.LoginForm import LoginForm
@@ -11,6 +11,7 @@ import sys
 
 if sys.version_info.major == 3 and sys.version_info.minor >= 10:
     import collections
+
     setattr(collections, "MutableMapping", collections.abc.MutableMapping)
 from flask_socketio import SocketIO, emit
 
@@ -23,74 +24,81 @@ Session(app)
 
 socketio = SocketIO(app)
 
+
 @socketio.on('enemigo')
 def enemigo():
     print("aperece enemigo")
 
+
 @app.get('/login')
 def login_template():
-    if  session.get("nombre"):
-        return render_template('inicial.html')   
+    if session.get("nombre"):
+        return render_template('inicial.html')
     form = LoginForm()
     return render_template('login.html', form=form)
 
+
 @app.post('/login')
 def login():
-
     form = LoginForm();
-    it=0;
-    #if form.validate_form(request):
+    it = 0;
+    # if form.validate_form(request):
     sql1 = "select id from jugador where nombre_de_usario = %s and contrasena = %s ";
-    record1=request.form.get('nombre');
-    record2=request.form.get('contrasena');
-    my_cursor.execute(sql1,(record1 ,record2));
+    record1 = request.form.get('nombre');
+    record2 = request.form.get('contrasena');
+    my_cursor.execute(sql1, (record1, record2));
     result = my_cursor.fetchall()
-    
+
     for row in result:
         print(row)
-        it+=1;
-    if it>0 :
-         print('login')
-         session["nombre"] = request.form.get("nombre")
-         return render_template('inicial.html')     
+        it += 1;
+    if it > 0:
+        print('login')
+        session["nombre"] = request.form.get("nombre")
+        return render_template('inicial.html')
 
     else:
-        print('no login')     
+        print('no login')
+
 
 @app.get('/register')
 def register_template():
-    if  session.get("nombre"):
-        return render_template('inicial.html')  
+    if session.get("nombre"):
+        return render_template('inicial.html')
     form = RegisterForm()
-    return render_template('register.html', form=form)     
+    return render_template('register.html', form=form)
+
 
 @app.post('/register')
 def register():
     form = RegisterForm()
-    if form.validate_form(request): 
+    if form.validate_form(request):
         sql1 = "insert into jugador(nombre_de_usario,contrasena,email) values (%s, %s, %s)";
-        record1 = ( request.form.get('nombre'), request.form.get('contrasena'),  request.form.get('email'));
-        my_cursor.execute(sql1,record1 );
+        record1 = (request.form.get('nombre'), request.form.get('contrasena'), request.form.get('email'));
+        my_cursor.execute(sql1, record1);
         mybd.commit();
-        session["nombre"] = request.form.get("nombre")        
+        session["nombre"] = request.form.get("nombre")
         my_cursor.execute("select * from jugador")
         for jg in my_cursor:
             print(jg)
-        return redirect(url_for('home')) 
+        return redirect(url_for('home'))
+
 
 @app.get('/game')
 def game():
     if not session.get("nombre"):
-        print('false - game');    
-        return render_template('index.html') 
+        print('false - game');
+        return render_template('index.html')
     print('true - game');
-    return render_template('juego.html')    
+    return render_template('juego.html')
+
 
 @app.get('/initial')
 def home():
     if not session.get("nombre"):
-        return render_template('index.html')   
-    return render_template('inicial.html')      
+        return render_template('index.html')
+    return render_template('inicial.html')
+
 
 @app.get('/logout')
 def logout():
@@ -102,42 +110,47 @@ def logout():
 def initial():
     return render_template('index.html')
 
+
 @app.get('/opciones')
 def opciones():
     if not session.get("nombre"):
-        return render_template('index.html')  
+        return render_template('index.html')
 
-    form = camb();     
+    form = camb();
     return render_template('opcion.html', form=form)
+
 
 @app.post('/opciones')
 def opc():
     if request.form.get('nombre').isalpha():
         form = camb();
-        if  form.validate_form(request):
-            my_cursor.execute("update jugador set nombre_de_usario = %s where nombre_de_usario = %s ", (request.form.get('nombre'),session.get("nombre")));
+        if form.validate_form(request):
+            my_cursor.execute("update jugador set nombre_de_usario = %s where nombre_de_usario = %s ",
+                              (request.form.get('nombre'), session.get("nombre")));
             mybd.commit();
             session["nombre"] = request.form.get('nombre');
             my_cursor.execute("select * from jugador")
             for jg in my_cursor:
-               print(jg)
-    return render_template('inicial.html')         
+                print(jg)
+    return render_template('inicial.html')
+
 
 @app.get('/eligir')
 def eligir():
     if not session.get("nombre"):
-        return render_template('index.html')   
+        return render_template('index.html')
     return render_template('eligir.html')
+
 
 @socketio.on('enemigo')
 def enemigo():
     print('enemigo')
 
+
 @socketio.on('connect')
 def connect():
     session['sid'] = request.sid
 
+
 if __name__ == '__main__':
-
     socketio.run(app, allow_unsafe_werkzeug=True)
-
